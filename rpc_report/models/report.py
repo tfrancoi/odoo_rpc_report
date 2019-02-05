6eb47b59cc0342a2b71e6005822693a2636124e8
Thibault Francois <tfr@openerp.com>
2019-02-05 16:09:41 +0100

    [FIX] Fix database selection during the request for asset: If there is more then one database in the list odoo does not know which database to select when wkhtmltopdf ask for the assets. As workaround the method force the authenticate to fill the session file with the right information and save it to the filesystem before calling wkhtmltopdf

--- rpc_report/models/report.py
+++ rpc_report/models/report.py
@@ -1,12 +1,25 @@
 import base64
 
 from odoo import api, models
+from odoo.http import request, root
 
 class RPCReport(models.Model):
     _inherit = 'ir.actions.report'
 
+    def _force_session_store(self):
+        """
+            Authenticate like a normal user to have all the needed info
+            in the session and save the session on the filesystem prior to call
+            wkhtmltopdf
+        """
+        password = request.params['args'][2]
+        db = request.params['args'][0]
+        request.session.authenticate(db, self.env.user.login, password)
+        root.session_store.save(request.session)
+
     @api.multi
     def render_rpc(self, res_ids, data):
+        self._force_session_store()
         res = self.render(res_ids, data)
         if isinstance(res, tuple):
             return base64.b64encode(res[0]), res[1]
